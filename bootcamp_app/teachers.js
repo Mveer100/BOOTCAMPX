0e67952b1127a292bb01a6d8ebf0fea2adc592e2
Maxwell Veer <maxveer@gmail.com>
2022-04-26 14:12:28 -0300

    test test

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,28 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id
+  WHERE cohorts.name = '$1 || 'JUL02'}'
+  ORDER BY teacher;
+  `;
+
+pool.query(queryString, values).then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  });
+});
